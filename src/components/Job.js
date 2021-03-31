bd65aa16af8d2cce4ec33f80af0871382ffc39c3
MarcoCarpo <marco.carpo95@gmail.com>
2021-03-31 10:10:41 +0200

    complete mobile styling

--- src/components/Job.js
+++ src/components/Job.js
@@ -15,22 +15,25 @@ const Job = ({
         <img src={logo} alt={`${company} logo`} />
       </div>
       <div className="job__upper">
-        <h3 className="job__company">{company}</h3>
-        <div className="job__new-feat">
-          {newJob ? (
-            <span className="job__feat job__feat--new">NEW!</span>
-          ) : null}
-          {featured ? (
-            <span className="job__feat job__feat--featured">FEATURED</span>
-          ) : null}
+        <div className="job__upper-company">
+          <h3 className="job__company">{company}</h3>
+          <div className="job__new-feat">
+            {newJob ? (
+              <span className="job__feat job__feat--new">NEW!</span>
+            ) : null}
+            {featured ? (
+              <span className="job__feat job__feat--featured">FEATURED</span>
+            ) : null}
+          </div>
+        </div>
+        <h2 className="job__position">{position}</h2>
+        <div className="job__info">
+          {jobInfo.map((info, index) => (
+            <span key={index}>{info}</span>
+          ))}
         </div>
       </div>
-      <h2 className="job__position">{position}</h2>
-      <div className="job__info">
-        {jobInfo.map((info, index) => (
-          <span key={index}>{info}</span>
-        ))}
-      </div>
+      <hr className="job__split" />
       <div className="job__tablets">
         {jobTablets.map((tablet, index) => (
           <button key={index} className="job__tablet">
